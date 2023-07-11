59eb273c5f5b2a07570f05d789c6072f6deab801
Jaimin <Jaiminjagdish@gmail.com>
2023-07-11 13:47:23 +0530

    Acknowledgements Added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,43 @@
-const socket=io()
+const socket = io();
 
-socket.on('message',(msg)=>{
-    console.log(msg)
-})
+const $messageForm = document.getElementById("message-form");
+const $messageInput = $messageForm.querySelector("input");
+const $messageFormBtn = $messageForm.querySelector("button");
+const $locationButton = document.getElementById("share-location");
 
-document.getElementById('message-form').addEventListener('submit',(e)=>{
-    e.preventDefault();
+socket.on("message", (msg) => {
+  console.log(msg);
+});
 
-    //using name property on inpt field
-    const message=e.target.elements.message.value;
-    socket.emit('newMessage',message);
-})
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormBtn.setAttribute("disabled", "disabled");
+
+  //using name property on input field
+  const message = e.target.elements.message.value;
+  socket.emit("newMessage", message, () => {
+    $messageFormBtn.removeAttribute("disabled");
+    $messageInput.focus();
+    $messageInput.value = "";
+    console.log("Delivered");
+  });
+});
+
+$locationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Your browser doesn't support this feature");
+  }
+  $locationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    return socket.emit(
+      "sendLocation",
+      { lat: position.coords.latitude, long: position.coords.longitude },
+      (ack_msg) => {
+        $locationButton.removeAttribute("disabled");
+        console.log(ack_msg);
+      }
+    );
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,32 +1,40 @@
-import express from "express"
-import path from "path"
-import {Server} from "socket.io"
-import http from "http"
+import express from "express";
+import path from "path";
+import { Server } from "socket.io";
+import http from "http";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-
-const app=express();
-const server=http.createServer(app)
-const io=new Server(server)
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.json());
 
-io.on('connection',(socket)=>{
-    const welcomeMsg="Welcome"
-    socket.emit('message',welcomeMsg);
+io.on("connection", (socket) => {
+  const welcomeMsg = "Welcome";
+  socket.emit("message", welcomeMsg);
+  socket.broadcast.emit("message", "A new user has joined the Chat!");
 
-    socket.on('newMessage',(clientMessage)=>{
-        io.emit('message',clientMessage);
-    })
+  socket.on("newMessage", (clientMessage, ack_callback) => {
+    io.emit("message", clientMessage);
+    ack_callback();
+  });
 
-})
+  socket.on("sendLocation", (loc, ack_callback) => {
+    io.emit("message", `https://google.com/maps?q=${loc.lat},${loc.long}`);
+    ack_callback("Location Shared");
+  });
 
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+});
 
-app.use(express.static(path.join(__dirname,"../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
-server.listen("3000",()=>{
-    console.log("App is running");
-})
+server.listen("3000", () => {
+  console.log("App is running");
+});
